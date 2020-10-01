f056aef759325cee8afba58d4899f90186bcf326
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-01 21:00:47 +0300

    added HW vue lesson 1 'Sokovita Hurma'

--- hw_2_01/src/App.vue
+++ hw_2_01/src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <InfoBox
+    logo="https://ppobzor.ru/wp-content/uploads/2018/06/recreativ-logo.jpg"
+    imgSrc="https://golapravda.blog/wp-content/uploads/2019/12/103-9-1024x681.jpg"
+    title="Соковита хурма"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import InfoBox from "./components/InfoBox";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InfoBox
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ hw_2_01/src/components/InfoBox/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="container">
+    <div id="header">
+      <div class="header_block logo"><img :src="logo" alt="logo" /></div>
+      <div class="header_block">Рекламна мережа</div>
+    </div>
+    <img :src="imgSrc" />
+    <div>
+      <a href="">{{ title }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoBox",
+  props: {
+    logo: {
+      type: String,
+    },
+    imgSrc: {
+      type: String,
+      default:
+        "https://lh3.googleusercontent.com/proxy/NRCaCLo705mO-qd3YxKKAC-UhqZZqqdCYL_MjR9lWIXeNgDPeFK_G0AfziGVZPPsG7lesVyZQroOIQhCspGZVpgmPbHdq78Oqm_wk9YXEVET8ydL0VNj9SPKGG3HfGHujwcegNqkdNl5_sOnjmGl4A",
+    },
+    title: {
+      type: String,
+      default: "No title",
+    },
+  },
+};
+</script>
+    
+<style scoped>
+#container {
+  background: #f3f3f3;
+  width: 200px;
+  height: auto;
+}
+#header {
+  background: #424252;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.header_block {
+  color: white;
+}
+.logo {
+  width: 70px;
+}
+img {
+  width: 100%;
+}
+a {
+  text-decoration: none;
+}
+</style>
