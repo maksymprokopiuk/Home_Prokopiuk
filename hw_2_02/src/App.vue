5b9d256db42dfaa4840b1b05df8b694b49fce12e
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-02 23:21:55 +0300

    added HW vue lesson 2 'Bank'

---
+++ hw_2_02/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <bank/>
+  </div>
+</template>
+
+<script>
+import Bank from './components/Bank'
+
+export default {
+  name: 'App',
+  components: {
+    Bank
+  }
+}
+</script>
+
+<style>
+
+</style>
