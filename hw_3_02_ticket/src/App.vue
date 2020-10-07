bcc4a05c793ce0bc9db808de5e674d7026c9c57c
Maksym Prokopiuk <maksym.prokopiuk@gmail.com>
2020-10-07 15:29:10 +0300

    added hw lesson 3 ticket

---
+++ hw_3_02_ticket/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <ticket/>
+  </div>
+</template>
+
+<script>
+import Ticket from '@/components/Ticket'
+
+export default {
+  name: 'App',
+  components: {
+    Ticket
+  }
+}
+</script>
+
+<style>
+body {
+    font-family: 'Roboto', sans-serif;
+  }
+</style>
